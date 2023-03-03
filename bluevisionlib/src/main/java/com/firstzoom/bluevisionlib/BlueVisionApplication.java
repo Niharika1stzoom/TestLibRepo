b42418287183202a5cee9b17c946756fc602ada7
Niharika <npmehta29@gmail.com>
2023-03-03 15:59:17 +0530

    application class removed

--- bluevisionlib/src/main/java/com/firstzoom/bluevisionlib/BlueVisionApplication.java
+++ bluevisionlib/src/main/java/com/firstzoom/bluevisionlib/BlueVisionApplication.java
@@ -3,7 +3,8 @@ package com.firstzoom.bluevisionlib;
 import android.app.Application;
 
 import dagger.hilt.android.HiltAndroidApp;
-
+/*
 @HiltAndroidApp
 public class BlueVisionApplication extends Application {
 }
+*/
